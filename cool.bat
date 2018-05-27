346cfe582b8c776fdf94a240817f436b1f7102c5
Mrcorbie <33270436+MrCorbie@users.noreply.github.com>
2018-05-27 18:48:18 +1200

    Create cool.bat

---
+++ cool.bat
@@ -0,0 +1,79 @@
+@Echo off
+color 17
+echo                                                                                                              WINDOWS %random%
+echo                                                                                              windowns has bad to end due to an unexpected error
+echo                                                                                                       memory dump will now commence
+ping 1.1.1.1 -n 1 -w 5000>nul
+for /l %%A in (100, -1 0) do (
+echo %random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%
+dir C:
+echo %random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%
+)
+cls
+ping 1.1.1.1 -n 1 -w 3000>nul
+echo                                                                                               RUNDLL32 HAS CAUSED THE OS TO STOP RESPONDING
+echo                                                                                                    RUNDLL32 WILL NOW BE DELETED
+ping 1.1.1.1 -n 1 -w 5000>nul
+for /l %%A in (200, -1, 0) do (
+echo %random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%
+echo                                                                                               RUNDLL32 HAS NOW BEEN REMOVED FROM SYSTEM32
+)
+ping 1.1.1.1 -n 1 -w 1000>nul
+cls
+echo                                                                                                Explorer.exe has stopped responding
+echo                                                                                                    Explorer.exe must now be deleted
+ping 1.1.1.1 -n 1 -w 3000>nul
+for /l %%A in (3 -1, 0) do (
+dir C:\windows
+echo                                                                                                            OVVL MAXX
+)
+ping 1.1.1.1 -n 1 -w 5000>nul
+echo please press any key to restart your computer
+pause >nul
+echo SYNTAX-ERROR:
+ping 1.1.1.1 -n 1 -w 500>nul
+echo SYNTAX-ERROR:
+ping 1.1.1.1 -n 1 -w 500>nul
+echo SYNTAX-ERROR:
+ping 1.1.1.1 -n 1 -w 500>nul
+echo SYNTAX-ERROR:
+ping 1.1.1.1 -n 1 -w 500>nul
+echo SYNTAX-ERROR:
+ping 1.1.1.1 -n 1 -w 500>nul
+echo SYNTAX-ERROR:
+ping 1.1.1.1 -n 1 -w 500>nul
+echo SYNTAX-ERROR:
+ping 1.1.1.1 -n 1 -w 500>nul
+echo SYNTAX-ERROR:
+ping 1.1.1.1 -n 1 -w 500>nul
+echo SYNTAX-ERROR:
+ping 1.1.1.1 -n 1 -w 500>nul
+echo SYNTAX-ERROR:
+ping 1.1.1.1 -n 1 -w 500>nul
+echo SYNTAX-ERROR:
+ping 1.1.1.1 -n 1 -w 500>nul
+echo SYNTAX-ERROR:
+ping 1.1.1.1 -n 1 -w 500>nul
+echo SYNTAX-ERROR:
+ping 1.1.1.1 -n 1 -w 500>nul
+echo SYNTAX-ERROR:
+ping 1.1.1.1 -n 1 -w 500>nul
+echo SYNTAX-ERROR:
+ping 1.1.1.1 -n 1 -w 500>nul
+echo SYNTAX-ERROR:
+ping 1.1.1.1 -n 1 -w 500>nul
+echo SYNTAX-ERROR:
+ping 1.1.1.1 -n 1 -w 500>nul
+echo SYNTAX-ERROR:
+ping 1.1.1.1 -n 1 -w 500>nul
+echo SYNTAX-ERROR:
+ping 1.1.1.1 -n 1 -w 500>nul
+echo SYNTAX-ERROR:
+ping 1.1.1.1 -n 1 -w 500>nul
+echo SYNTAX-ERROR:
+ping 1.1.1.1 -n 1 -w 500>nul
+echo SYNTAX-ERROR:
+ping 1.1.1.1 -n 1 -w 500>nul
+echo SYNTAX-ERROR:
+ping 1.1.1.1 -n 1 -w 500>nul
+echo SYNTAX-ERROR:
