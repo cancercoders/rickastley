41a859193b2bbec8ba08d22ef581f7e2a0ac8030
RealVaranox <30459307+RealVaranox@users.noreply.github.com>
2018-05-27 19:20:31 +1200

    Create images.bat

---
+++ images.bat
@@ -0,0 +1,242 @@
+@echo off
+
+start astley1.jpg
+timeout 1 /nobreak
+start astley2.jpg
+timeout 1 /nobreak
+start astley3.jpg
+timeout 1 /nobreak
+start astley4.jpg
+timeout 1 /nobreak
+start astley5.jpg
+timeout 1 /nobreak
+start astley6.jpg
+timeout 1 /nobreak
+start astley7.jpg
+timeout 1 /nobreak
+start astley8.jpg
+start astley1.jpg
+timeout 1 /nobreak
+start astley2.jpg
+timeout 1 /nobreak
+start astley3.jpg
+timeout 1 /nobreak
+start astley4.jpg
+timeout 1 /nobreak
+start astley5.jpg
+timeout 1 /nobreak
+start astley6.jpg
+timeout 1 /nobreak
+start astley7.jpg
+timeout 1 /nobreak
+start astley8.jpg
+start astley1.jpg
+timeout 1 /nobreak
+start astley2.jpg
+timeout 1 /nobreak
+start astley3.jpg
+timeout 1 /nobreak
+start astley4.jpg
+timeout 1 /nobreak
+start astley5.jpg
+timeout 1 /nobreak
+start astley6.jpg
+timeout 1 /nobreak
+start astley7.jpg
+timeout 1 /nobreak
+start astley8.jpg
+start astley1.jpg
+timeout 1 /nobreak
+start astley2.jpg
+timeout 1 /nobreak
+start astley3.jpg
+timeout 1 /nobreak
+start astley4.jpg
+timeout 1 /nobreak
+start astley5.jpg
+timeout 1 /nobreak
+start astley6.jpg
+timeout 1 /nobreak
+start astley7.jpg
+timeout 1 /nobreak
+start astley8.jpg
+start astley1.jpg
+timeout 1 /nobreak
+start astley2.jpg
+timeout 1 /nobreak
+start astley3.jpg
+timeout 1 /nobreak
+start astley4.jpg
+timeout 1 /nobreak
+start astley5.jpg
+timeout 1 /nobreak
+start astley6.jpg
+timeout 1 /nobreak
+start astley7.jpg
+timeout 1 /nobreak
+start astley8.jpg
+start astley1.jpg
+timeout 1 /nobreak
+start astley2.jpg
+timeout 1 /nobreak
+start astley3.jpg
+timeout 1 /nobreak
+start astley4.jpg
+timeout 1 /nobreak
+start astley5.jpg
+timeout 1 /nobreak
+start astley6.jpg
+timeout 1 /nobreak
+start astley7.jpg
+timeout 1 /nobreak
+start astley8.jpg
+start astley1.jpg
+timeout 1 /nobreak
+start astley2.jpg
+timeout 1 /nobreak
+start astley3.jpg
+timeout 1 /nobreak
+start astley4.jpg
+timeout 1 /nobreak
+start astley5.jpg
+timeout 1 /nobreak
+start astley6.jpg
+timeout 1 /nobreak
+start astley7.jpg
+timeout 1 /nobreak
+start astley8.jpg
+start astley1.jpg
+timeout 1 /nobreak
+start astley2.jpg
+timeout 1 /nobreak
+start astley3.jpg
+timeout 1 /nobreak
+start astley4.jpg
+timeout 1 /nobreak
+start astley5.jpg
+timeout 1 /nobreak
+start astley6.jpg
+timeout 1 /nobreak
+start astley7.jpg
+timeout 1 /nobreak
+start astley8.jpg
+start astley1.jpg
+timeout 1 /nobreak
+start astley2.jpg
+timeout 1 /nobreak
+start astley3.jpg
+timeout 1 /nobreak
+start astley4.jpg
+timeout 1 /nobreak
+start astley5.jpg
+timeout 1 /nobreak
+start astley6.jpg
+timeout 1 /nobreak
+start astley7.jpg
+timeout 1 /nobreak
+start astley8.jpg
+start astley1.jpg
+timeout 1 /nobreak
+start astley2.jpg
+timeout 1 /nobreak
+start astley3.jpg
+timeout 1 /nobreak
+start astley4.jpg
+timeout 1 /nobreak
+start astley5.jpg
+timeout 1 /nobreak
+start astley6.jpg
+timeout 1 /nobreak
+start astley7.jpg
+timeout 1 /nobreak
+start astley8.jpg
+start astley1.jpg
+timeout 1 /nobreak
+start astley2.jpg
+timeout 1 /nobreak
+start astley3.jpg
+timeout 1 /nobreak
+start astley4.jpg
+timeout 1 /nobreak
+start astley5.jpg
+timeout 1 /nobreak
+start astley6.jpg
+timeout 1 /nobreak
+start astley7.jpg
+timeout 1 /nobreak
+start astley8.jpg
+start astley1.jpg
+timeout 1 /nobreak
+start astley2.jpg
+timeout 1 /nobreak
+start astley3.jpg
+timeout 1 /nobreak
+start astley4.jpg
+timeout 1 /nobreak
+start astley5.jpg
+timeout 1 /nobreak
+start astley6.jpg
+timeout 1 /nobreak
+start astley7.jpg
+timeout 1 /nobreak
+start astley8.jpg
+start astley1.jpg
+timeout 1 /nobreak
+start astley2.jpg
+timeout 1 /nobreak
+start astley3.jpg
+timeout 1 /nobreak
+start astley4.jpg
+timeout 1 /nobreak
+start astley5.jpg
+timeout 1 /nobreak
+start astley6.jpg
+timeout 1 /nobreak
+start astley7.jpg
+timeout 1 /nobreak
+start astley8.jpg
+start astley1.jpg
+timeout 1 /nobreak
+start astley2.jpg
+timeout 1 /nobreak
+start astley3.jpg
+timeout 1 /nobreak
+start astley4.jpg
+timeout 1 /nobreak
+start astley5.jpg
+timeout 1 /nobreak
+start astley6.jpg
+timeout 1 /nobreak
+start astley7.jpg
+timeout 1 /nobreak
+start astley8.jpg
+start astley1.jpg
+timeout 1 /nobreak
+start astley2.jpg
+timeout 1 /nobreak
+start astley3.jpg
+timeout 1 /nobreak
+start astley4.jpg
+timeout 1 /nobreak
+start astley5.jpg
+timeout 1 /nobreak
+start astley6.jpg
+timeout 1 /nobreak
+start astley7.jpg
+timeout 1 /nobreak
+start astley8.jpg
+start astley1.jpg
+timeout 1 /nobreak
+start astley2.jpg
+timeout 1 /nobreak
+start astley3.jpg
+timeout 1 /nobreak
+start astley4.jpg
+timeout 1 /nobreak
+start astley5.jpg
+timeout 1 /nobreak
+start astley6.jpg
+timeout 1 /nobreak
+start astley7.jpg
+timeout 1 /nobreak
+start astley8.jpg
